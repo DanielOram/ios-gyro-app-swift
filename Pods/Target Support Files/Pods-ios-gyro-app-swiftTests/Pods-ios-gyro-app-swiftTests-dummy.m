75f7fdb0c1fb0f9f3f5cad83d9ae9c37e39975b5
Daniel Oram <danieloramnz@gmail.com>
2016-08-11 17:50:02 +1200

    Revert "install MotionKit using cocoapods"
    This reverts commit 0351e1b0e2484113f704ecaaabf512cf6bdbb148.

--- Pods/Target Support Files/Pods-ios-gyro-app-swiftTests/Pods-ios-gyro-app-swiftTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ios_gyro_app_swiftTests : NSObject
-@end
-@implementation PodsDummy_Pods_ios_gyro_app_swiftTests
-@end
--- Pods/Target Support Files/Pods-ios-gyro-app-swiftUITests/Pods-ios-gyro-app-swiftUITests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_ios_gyro_app_swiftUITestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_ios_gyro_app_swiftUITestsVersionString[];
-

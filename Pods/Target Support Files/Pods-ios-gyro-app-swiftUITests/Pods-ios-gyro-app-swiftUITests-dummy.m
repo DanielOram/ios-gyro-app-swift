0351e1b0e2484113f704ecaaabf512cf6bdbb148
Daniel Oram <danieloramnz@gmail.com>
2016-08-11 16:45:05 +1200

    install MotionKit using cocoapods

---
+++ Pods/Target Support Files/Pods-ios-gyro-app-swiftUITests/Pods-ios-gyro-app-swiftUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ios_gyro_app_swiftUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ios_gyro_app_swiftUITests
+@end
---
+++ Pods/Target Support Files/Pods-ios-gyro-app-swiftUITests/Pods-ios-gyro-app-swiftUITests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_ios_gyro_app_swiftUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ios_gyro_app_swiftUITestsVersionString[];
+
